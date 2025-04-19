0d13d3e3917da0aa04079cbb671e44ad0c3537f2
Vlad Dubov <dubov.web@gmail.com>
2025-04-19 22:51:42 +0300

    iter1: add first implementation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" && r.Method == "POST" {
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+			return
+		}
+
+		re, err := regexp.MatchString("^/([a-zA-Z]+$)", r.URL.Path)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if re && r.Method == "GET" {
+			w.Header().Add("Location", "https://practicum.yandex.ru/")
+			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+	})
+	http.ListenAndServe("localhost:8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hotspurs/go-advance-shortener
+
+go 1.24.2
