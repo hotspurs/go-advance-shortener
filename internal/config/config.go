35ec6171acde244563a44f7737069be7c8c40fd7
Vlad Dubov <dubov.web@gmail.com>
2025-04-26 11:43:40 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -13,7 +14,18 @@ func Init() *Config {
 	address := flag.String("a", "localhost:8080", "HTTP server address")
 	baseURL := flag.String("b", "http://localhost:8080", "Base URL")
 
+	envAddress := os.Getenv("SERVER_ADDRESS")
+	envBaseUrl := os.Getenv("BASE_URL")
+
 	flag.Parse()
+
+	if envAddress != "" {
+		address = &envAddress
+	}
+
+	if envBaseUrl != "" {
+		address = &envBaseUrl
+	}
 
 	cfg := &Config{
 		Address: *address,
