9f9dea52973d8ed248a43bcbf47bd268293b25ad
Vlad Dubov <dubov.web@gmail.com>
2025-05-29 23:20:17 +0300

    iter7

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/hotspurs/go-advance-shortener/internal/config"
 	"github.com/hotspurs/go-advance-shortener/internal/rand"
 	"io"
@@ -11,6 +13,14 @@ import (
 type Storage interface {
 	Add(key string, value string)
 	Get(key string) string
+}
+
+type Request struct {
+	Url string `json:"url"`
+}
+
+type Response struct {
+	Result string `json:"result"`
 }
 
 func GetHandler(data Storage) http.HandlerFunc {
@@ -33,5 +43,33 @@ func GenerateHandler(data Storage, config *config.Config) http.HandlerFunc {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte(config.BaseURL + "/" + short))
+	})
+}
+
+func ShortenHandler(data Storage, config *config.Config) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var req Request
+		var buf bytes.Buffer
+		_, err := buf.ReadFrom(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if err = json.Unmarshal(buf.Bytes(), &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		short := rand.String(8)
+		data.Add(short, req.Url)
+		var res Response
+		res.Result = config.BaseURL + "/" + short
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		resp, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(resp)
 	})
 }
